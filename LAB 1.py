492665f62c39186bd8170ecb6ededcde67e8260a
somilewis <87447136+somilewis@users.noreply.github.com>
2023-06-02 07:47:25 -0700

    Add files via upload

---
+++ LAB 1.py
@@ -0,0 +1,24 @@
+# Lewis Muthomi
+# 1250 01
+# 08-31-2022
+# lab 1
+
+# This program get user first name and last name
+# user greeting
+print('This program will display a message')
+print()
+
+# Get user first name
+firstName = input('please enter your first name: ')
+
+# Get user second name
+secondName = input('please enter your second name: ')
+print()
+
+# display message
+print('hello',firstName, secondName)
+print('welcome to python!') 
+print()
+
+# exits message
+print('have a nice day!')
